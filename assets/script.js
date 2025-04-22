0c02a7a714c28461a7994ff524ba7c1ffbb59f25
vitor <vitorc0715@gmail.com>
2025-04-21 21:15:50 -0300

    adicionando estilização ao projeto

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const openBtn = document.getElementById('openBtn');
+    const surprise = document.getElementById('surprise');
+  
+    openBtn.addEventListener('click', function () {
+      surprise.classList.remove('hidden');
+      openBtn.style.display = 'none';
+    });
+  });
+  
